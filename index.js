41ad189da9a6c6a6ff0a9a7d5daebf2c5dbd251e
Juana Copello <copellojuana@gmail.com>
2024-01-14 10:29:04 -0300

    grafico funcionando - arg

--- index.js
+++ index.js
@@ -1,86 +1,67 @@
 //Para hacer carrera animada: https://www.amcharts.com/demos/bar-chart-race/
 //https://stackoverflow.com/questions/72941086/amcharts-5-bar-chart-race-valueaxis-reset-on-chart-replay
 
-/* Chart code */
-// Create root element
+
 // https://www.amcharts.com/docs/v5/getting-started/#Root_element
 let root = am5.Root.new("chart-cont");
 
-const myTheme = am5.Theme.new(root);
-
-// Move minor label a bit down
-myTheme.rule("AxisLabel", ["minor"]).setAll({
-  dy: 1,
+root.dateFormatter.setAll({
+  dateFormat: "yyyy",
+  dateFields: ["valueX"]
 });
 
-// Tweak minor grid opacity
-// myTheme.rule("Grid", ["minor"]).setAll({
-//   strokeOpacity: 0.08,
-// });
-
-// Set themes
-// https://www.amcharts.com/docs/v5/concepts/themes/
-// root.setThemes([
-//   am5themes_Animated.new(root),
-//   myTheme
-// ]);
-
 let parsedData;
-let allCountries
+let allCountries;
 
+// Procesamiento de datos
+let processor = am5.DataProcessor.new(root, {
+  numericFields: ["valor_en_porcentaje"],
+  dateFormat: "yyyy",
+  dateFields: ["anio"]
+  // dateFields: ["anio"],
+});
 
-    // Process data
-    let processor = am5.DataProcessor.new(root, {
-      dateFields: ["anio"],
-      dateFormat: "yyyy",
-      numericFields: ["valor_en_porcentaje"],
-    });
-
-
+//Fetch de datos CSV
 am5.net
-  .load("/data/energia_baja_carbono.csv")
+  .load("/data/energia_baja_carbono_arg.csv")
   .then((data) => {
-    console.log(data.response);
-    parsedData = am5.CSVParser.parse(data.response, {
+    let fetchedData = data.response;
+    console.log(fetchedData);
+
+    //Parseo de datos CSV
+    parsedData = am5.CSVParser.parse(fetchedData, {
       delimiter: ";",
       reverse: true,
       skipEmpty: true,
       useColumnNames: true,
-      process: (row) => {
-        // Convert timestamp to a Date object
-        row.anio = new Date(parseInt(row.anio));
-
-        return row;
-      },
     });
-    console.log(parsedData);
+
+    console.log("parsed data", parsedData);
 
     processor.processMany(parsedData);
 
-    allCountries = [...new Set(parsedData.map(item => item.iso3))];
-    allCountries.sort()
-    allCountries.forEach(country => {
-      document.getElementById('countryForm').innerHTML += `
+    //Busco los paises para hacer menu dropdown del selector
+    allCountries = [...new Set(parsedData.map((item) => item.iso3))];
+    allCountries.sort(); //Ordenado alfabeticamente
+    //Instanciado en HTML
+    allCountries.forEach((country) => {
+      document.getElementById("countryForm").innerHTML += `
         <label><input class="input-paises" type="checkbox" name="countries" value="${country}">${country}</label>
       `;
+    });
 
-    })
     series.data.setAll(parsedData);
   })
   .catch((result) => {
-    console.log("Error loading " + result.xhr.responseURL);
+    console.log("Error en carga de datos: " + result.xhr.responseURL);
   });
 
-  console.log("paises fuera", allCountries)
-
-// Create chart
+//Creo el gráfico Chart
 // https://www.amcharts.com/docs/v5/charts/xy-chart/
 let chart = root.container.children.push(
   am5xy.XYChart.new(root, {
     panX: false,
     panY: false,
-    // wheelX: "panX",
-    // wheelY: "zoomX",
     paddingLeft: 0,
   })
 );
@@ -96,30 +77,15 @@ let cursor = chart.set(
 
 cursor.lineY.set("visible", false);
 
-// Create axes
-// https://www.amcharts.com/docs/v5/charts/xy-chart/axes/
-let xAxis = chart.xAxes.push(
-  am5xy.DateAxis.new(root, {
-    maxDeviation: 0,
-    baseInterval: {
-      timeUnit: "year",
-      count: 10,
-    },
-
-    renderer: am5xy.AxisRendererX.new(root, {
-      minorGridEnabled: true,
-      minGridDistance: 200,
-      minorLabelsEnabled: true,
-    }),
-    tooltip: am5.Tooltip.new(root, {}),
-  })
-);
-
-xAxis.set("minorDateFormats", {
-  // day: "dd",
-  // month: "MM",
-  year: "YYYY",
-});
+let xAxis = chart.xAxes.push(am5xy.DateAxis.new(root, {
+  baseInterval: { timeUnit: "year", count: 1 },
+  renderer: am5xy.AxisRendererX.new(root, {
+    // minGridDistance: 50,
+    // pan:"zoom",
+    // minorGridEnabled: true
+  }),
+  tooltip: am5.Tooltip.new(root, {})
+}));
 
 let yAxis = chart.yAxes.push(
   am5xy.ValueAxis.new(root, {
@@ -127,8 +93,12 @@ let yAxis = chart.yAxes.push(
   })
 );
 
-// Add series
-// https://www.amcharts.com/docs/v5/charts/xy-chart/series/
+
+/* Muchos line charts
+https://www.amcharts.com/demos/highlighting-line-chart-series-on-legend-hover/
+*/
+
+// Agregar serie por cada país seleccionado
 let series = chart.series.push(
   am5xy.LineSeries.new(root, {
     name: "Series",
@@ -141,14 +111,3 @@ let series = chart.series.push(
     }),
   })
 );
-
-
-
-
-const countriesArray = ["USA", "Canada", "UK", "Australia", /* Add more countries as needed */];
-
-    // Dynamically generate checkboxes based on the countries array
-  
-// const downloadCSV = () => {
-//   const csvData = 
-// }
